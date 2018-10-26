5a394b48d8c05c1eeeb0d223bb3f2e5e803e9749
Lorenzo <l.cozza@proplant.de>
2018-10-26 08:14:17 +0200

    download spring boot initializr

---
+++ src/main/java/com/springboot/beispiele/springbootBeispiele/SpringbootBeispieleApplication.java
@@ -0,0 +1,12 @@
+package com.springboot.beispiele.springbootBeispiele;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBeispieleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBeispieleApplication.class, args);
+	}
+}
---
+++ src/test/java/com/springboot/beispiele/springbootBeispiele/SpringbootBeispieleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.springboot.beispiele.springbootBeispiele;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootBeispieleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
