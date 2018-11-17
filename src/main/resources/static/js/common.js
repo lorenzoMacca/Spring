8e46b53c8722d8bfe2d0c8521ff9b5f6ec823d38
LorenzoMacca <lorenzo.cozza@hotmail.com>
2018-11-18 00:49:17 +0100

    add LocalDate and LocalTime

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -14,3 +14,30 @@ function executeAjaxCall(url, map, callback, dataForCallback){
 function getAll(url, callback, dataForCallback){
 	executeAjaxCall(url, new Map(), callback, dataForCallback);
 }
+
+function createAndFillTable(parentId, columns, data){
+	var htmlColumns = "";
+	columns.forEach(function(element) {
+		htmlColumns += "<th scope='col'>"+ element +"</th>"
+	});
+	var htmlDate = "";
+	for (var i = 0; i < data.length; i++) {
+		htmlDate +="<tr>";
+		for (var j = 0; j < data[i].length; j++) {
+			htmlDate += "<td>"+ data[i][j] +"</td>";
+		}
+		htmlDate +="</tr>";
+	}
+	var html = "";
+	html +="<table class='table table-hover'>"
+		     +"<thead>"
+		        +"<tr>"
+		           + htmlColumns
+		        +"</tr>"
+		     +"</thead>"
+		     +"<tbody>"
+	html += htmlDate;
+	html +="</tbody>"
+		  +"</table>"
+	$(parentId).html(html);
+}
--- src/main/resources/static/js/swimmController.js
+++ src/main/resources/static/js/swimmController.js
@@ -3,6 +3,14 @@ var INDOR_SWIM_GET_ALL = '/training/swim/activities';
 function updateViewGetAllSwimCB(data, dataForCallback){
 	console.log("[WHERE] - updateViewGetAllSwimCB");
 	console.log(JSON.stringify(data));
+	var columns = ['Date', 'Time', 'distance'];
+	var neededData = new Array(3);
+	for (var i = 0; i < data.length; i++) {
+		var tmp = [data[i].date, data[i].time, data[i].numberOfLaps];
+		neededData[i]=tmp;
+	}
+	console.log(JSON.stringify(neededData));
+	createAndFillTable(activityTable, columns, neededData);
 }
 
 $("#swimSectionMenuBtnId").click(function(){
